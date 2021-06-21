bb69bfc071ba0a8149b8256bdf8d444ff49c297b
Oleg Yapparov <oleg.yapparov@infor.com>
2021-06-21 23:33:07 +0200

    Project init

---
+++ kamenice_django/urls.py
@@ -0,0 +1,35 @@
+"""kamenice_django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import re_path
+from django.views.static import serve
+
+from ui import views as ui
+
+urlpatterns = [
+    # Client UI routes
+    url(r'^$', ui.home, name='home'),
+
+    # Admin section
+    url('admin/', admin.site.urls),
+
+    # Static
+    re_path(r'^assets/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
+]
