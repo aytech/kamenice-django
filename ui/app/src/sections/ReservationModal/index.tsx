e21ab396bc7727dd1394ac88a7ba50a2211b8c22
Oleg Yapparov <oyapparov@gmail.com>
2021-07-02 13:55:02 +0200

    convert form to Ant form

--- ui/app/src/sections/ReservationModal/index.tsx
+++ ui/app/src/sections/ReservationModal/index.tsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from "react"
-import { Button, DatePicker, Form, Modal, Select, TimePicker } from "antd"
+import React, { useEffect } from "react"
+import { Button, DatePicker, Form, Modal, Select } from "antd"
 import locale from "antd/es/date-picker/locale/cs_CZ"
 import { Reservation } from "../../lib/components/Reservation"
-import { ReserveDay, ReserveRange } from "../../lib/components/Room"
+import { ReserveRange } from "../../lib/components/Room"
 import { Moment } from "moment"
-import { RangeValue } from "rc-picker/lib/interface"
 import moment from "moment"
-import { defaultArrivalHour, defaultDepartureHour } from "../../lib/Constants"
 import { DrawerType } from "../../lib/Types"
 import { Store } from "rc-field-form/lib/interface"
 
@@ -29,70 +27,33 @@ export const ReservationModal = ({
   updateRange
 }: Props) => {
 
-  const [ selectedFromDay, setSelectedFromDay ] = useState<ReserveDay>()
-  const [ selectedToDay, setSelectedToDay ] = useState<ReserveDay>()
+  const getRangePickerDates = (range: ReserveRange | undefined): Array<Moment | null> => {
+    if (range === undefined) {
+      return []
+    }
+    const fromDate = [ range.from.year, range.from.month - 1, range.from.day ]
+    const toDate = [ range.to.year, range.to.month - 1, range.to.day ]
+    if (range.from.hour !== undefined) fromDate.push(range.from.hour)
+    if (range.from.minute !== undefined) fromDate.push(range.from.minute)
+    if (range.to.hour !== undefined) toDate.push(range.to.hour)
+    if (range.to.minute !== undefined) toDate.push(range.to.minute)
+    return [
+      moment(fromDate),
+      moment(toDate)
+    ]
+  }
   const [ form ] = Form.useForm()
   const initialValues: Store = {
+    dates: getRangePickerDates(range),
     type: range === undefined ? "binding" : range.type
   }
-  const dateFormat = "YYYY-MM-DD"
-  const timeFormat = "HH:mm"
+  const dateFormat = "YYYY-MM-DD HH:mm"
 
   useEffect(() => {
-    form.resetFields()
-  })
-
-  const getRangePicker = () => {
-    let from: Moment, to: Moment
-    if (selectedFromDay !== undefined) {
-      const { year, month, day } = selectedFromDay
-      from = moment([ year, month - 1, day ])
-    } else {
-      from = moment()
+    if (isOpen === true) {
+      form.resetFields()
     }
-    if (selectedToDay !== undefined) {
-      const { year, month, day } = selectedToDay
-      to = moment([ year, month - 1, day ])
-    } else {
-      to = moment()
-    }
-    return (
-      <RangePicker
-        value={ [
-          moment(from, dateFormat),
-          moment(to, dateFormat)
-        ] }
-        format={ dateFormat }
-        locale={ locale }
-        onChange={ (newRange: RangeValue<Moment>) => {
-          if (newRange !== null
-            && newRange[ 0 ] !== null
-            && newRange[ 1 ] !== null) {
-            setSelectedFromDay({
-              year: newRange[ 0 ].year(),
-              month: newRange[ 0 ].month() + 1,
-              day: newRange[ 0 ].date(),
-              hour: selectedFromDay?.hour === undefined ? defaultArrivalHour : selectedFromDay.hour,
-              minute: selectedFromDay?.minute === undefined ? 0 : selectedFromDay.minute
-            })
-            setSelectedToDay({
-              year: newRange[ 1 ].year(),
-              month: newRange[ 1 ].month() + 1,
-              day: newRange[ 1 ].date(),
-              hour: selectedToDay?.hour === undefined ? defaultDepartureHour : selectedToDay.hour,
-              minute: selectedToDay?.minute === undefined ? 0 : selectedToDay.minute
-            })
-          }
-        } } />
-    )
-  }
-
-  const getDefaultTime = (day: ReserveDay | undefined): Moment | undefined => {
-    if (day !== undefined && day.hour !== undefined && day.minute !== undefined) {
-      return moment([ day.year, day.month, day.day, day.hour, day.minute ])
-    }
-    return undefined
-  }
+  }, [ form, isOpen ])
 
   return (
     <Modal
@@ -117,32 +78,35 @@ export const ReservationModal = ({
           Zrušit
         </Button>,
         <Button
-          disabled={
-            selectedFromDay === undefined
-            || selectedToDay === undefined
-          }
           key="ok"
           onClick={ () => {
-            if (selectedFromDay !== undefined && selectedToDay !== undefined) {
-              const newRange: ReserveRange = {
-                from: selectedFromDay,
-                to: selectedToDay,
-                type: form.getFieldValue("type")
-              }
-              if (range !== undefined && range.id !== undefined) {
-                newRange.id = range.id
-              }
-              updateRange(newRange)
-              form.validateFields()
-                .then(() => {
-                  console.log('Submit form: ', form.getFieldsValue(true))
-                })
-                .catch((error: string) => {
-                  console.log('Oops: ', error)
-                })
-            }
-            // form.resetFields()
-            // close()
+            form.validateFields()
+              .then(() => {
+                const [ from, to ]: Array<Moment> = form.getFieldValue("dates")
+                const newRange: ReserveRange = {
+                  from: {
+                    year: from.year(),
+                    month: from.month() + 1,
+                    day: from.date(),
+                    hour: from.hour(),
+                    minute: from.minute()
+                  },
+                  to: {
+                    year: to.year(),
+                    month: to.month() + 1,
+                    day: to.date(),
+                    hour: to.hour(),
+                    minute: to.minute()
+                  },
+                  type: form.getFieldValue("type")
+                }
+                if (range !== undefined) newRange.id = range.id
+                updateRange(newRange)
+              })
+              .catch((error: string) => {
+                console.log('Oops: ', error)
+              })
+            close()
           } }>
           OK
         </Button>
@@ -154,40 +118,10 @@ export const ReservationModal = ({
         <Form.Item
           label="Datum Rezervace"
           name="dates">
-          { getRangePicker() }
-        </Form.Item>
-        <Form.Item
-          label="Čas příjezdu"
-          name="arrival">
-          <TimePicker
-            value={ getDefaultTime(selectedFromDay) }
-            format={ timeFormat }
+          <RangePicker
+            format={ dateFormat }
             locale={ locale }
-            onChange={ (value: Moment | null) => {
-              if (value !== null && selectedFromDay !== undefined) {
-                const from = selectedFromDay
-                from.hour = value.hour()
-                from.minute = value.minute()
-                setSelectedFromDay(from)
-              }
-            } }
-          />
-        </Form.Item>
-        <Form.Item
-          label="Čas odjezdu"
-          name="departure">
-          <TimePicker
-            value={ getDefaultTime(selectedToDay) }
-            format={ timeFormat }
-            onChange={ (value: Moment | null) => {
-              if (value !== null && selectedToDay !== undefined) {
-                const to = selectedToDay
-                to.hour = value.hour()
-                to.minute = value.minute()
-                setSelectedToDay(to)
-              }
-            } }
-          />
+            showTime />
         </Form.Item>
         <Form.Item
           label="Typ Rezervace"
