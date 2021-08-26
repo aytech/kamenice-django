6a9aec68c247dcbd7669da69cf4123ab6a65f6cd
Oleg Yapparov <oyapparov@gmail.com>
2021-08-26 10:05:59 +0200

    Add Guest to admin site

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 
+from api.models.Guest import Guest
 from api.models.Reservation import Reservation
 
 
+@admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     fields = ('from_date', 'to_date', 'guest', 'roommates', 'meal', 'notes', 'purpose', 'suite', 'type', 'deleted',
               'hash',)
     readonly_fields = ('hash',)
 
 
-admin.site.register(Reservation, ReservationAdmin)
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    pass
