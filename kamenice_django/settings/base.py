d6b2642ecff25de15ddc01294bcdebd9ad3a5aa4
Oleg Yapparov <oyapparov@gmail.com>
2021-09-08 15:54:58 +0200

    Revert DB to SQLite

--- kamenice_django/settings/base.py
+++ kamenice_django/settings/base.py
@@ -70,13 +70,17 @@ WSGI_APPLICATION = 'kamenice_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kamenice',
-        'USER': 'admin',
-        'PASSWORD': 'kamenice',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'kamenice',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'kamenice',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
 }
 
 # Password validation
