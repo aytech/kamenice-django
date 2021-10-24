4fc283945c0eb81aa7376ba583c3cd8949eaeb00
Oleg Yapparov <oyapparov@gmail.com>
2021-10-24 22:15:55 +0200

    Update settings migrations

--- api/models/Settings.py
+++ api/models/Settings.py
@@ -6,9 +6,9 @@ from api.models.BaseModel import BaseModel
 class Settings(BaseModel):
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     deleted = models.BooleanField(default=False)
-    municipality_fee = models.DecimalField(blank=True, decimal_places=2, default=0, max_digits=6, null=True)
-    price_breakfast = models.DecimalField(blank=True, decimal_places=2, default=0, max_digits=6, null=True)
-    price_halfboard = models.DecimalField(blank=True, decimal_places=2, default=0, max_digits=6, null=True)
+    municipality_fee = models.DecimalField(blank=True, decimal_places=2, default='0.00', max_digits=6, null=True)
+    price_breakfast = models.DecimalField(blank=True, decimal_places=2, default='0.00', max_digits=6, null=True)
+    price_halfboard = models.DecimalField(blank=True, decimal_places=2, default='0.00', max_digits=6, null=True)
     updated = models.DateTimeField(auto_now=True)
     user_avatar = models.CharField(blank=True, max_length=150, null=True)
     user_color = models.CharField(blank=True, max_length=50, null=True)
