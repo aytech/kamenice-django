b70c02bb894eca8be46fd72dc40f43b03a1f34cc
Oleg Yapparov <oyapparov@gmail.com>
2021-10-24 23:20:12 +0200

    Sort and search guests

--- ui/app/src/sections/Guests/index.tsx
+++ ui/app/src/sections/Guests/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { withRouter } from "react-router-dom"
-import { Button, Col, List, message, Row, Skeleton, Tooltip } from "antd"
+import { Button, Col, Input, List, message, Row, Skeleton, Tooltip } from "antd"
 import { GUESTS } from "../../lib/graphql/queries/Guests"
 import { Guests as GuestsData, Guests_guests } from "../../lib/graphql/queries/Guests/__generated__/Guests"
 import { UserAddOutlined } from "@ant-design/icons"
@@ -17,11 +17,29 @@ export const Guests = withRouter(() => {
 
   const { t } = useTranslation()
 
+  const [ filteredGuests, setFilteredGuests ] = useState<Guests_guests[]>([])
   const [ guests, setGuests ] = useState<Guests_guests[]>([])
 
   const { data, loading, refetch } = useQuery<GuestsData>(GUESTS, {
     onError: (reason: ApolloError) => message.error(reason.message)
   })
+
+  const sortGuests = (guestA: Guests_guests, guestB: Guests_guests) => {
+    return (guestA.name).localeCompare(guestB.name)
+  }
+
+  const onSearch = (value: string) => {
+    if (value.length < 1) {
+      setFilteredGuests(guests)
+    } else {
+      const foundGuests = guests.filter(guest => {
+        return guest.name.toLowerCase().indexOf(value.toLowerCase()) !== -1
+          || guest.surname.toLowerCase().indexOf(value.toLowerCase()) !== -1
+          || (guest.email !== null && guest.email.toLowerCase().indexOf(value.toLowerCase()) !== -1)
+      })
+      setFilteredGuests(foundGuests)
+    }
+  }
 
   useEffect(() => {
     const guestsList: Guests_guests[] = []
@@ -32,6 +50,7 @@ export const Guests = withRouter(() => {
         }
       })
       setGuests(guestsList)
+      setFilteredGuests(guestsList.sort(sortGuests))
     }
   }, [ data ])
 
@@ -49,18 +68,24 @@ export const Guests = withRouter(() => {
         <List
           bordered={ true }
           className="guests"
-          dataSource={ guests }
+          dataSource={ filteredGuests }
           footer={
             <Text disabled>&reg;{ t("company-name") }</Text>
           }
           header={ (
             <Row>
-              <Col lg={ 23 } md={ 22 } sm={ 20 } xs={ 20 }>
-                <h2>{ t("guests.list") }</h2>
+              <Col lg={ 10 } md={ 12 } sm={ 14 } xs={ 16 }>
+                <Input.Search
+                  allowClear
+                  enterButton
+                  onSearch={ onSearch }
+                  placeholder={ t("guests.search") } />
               </Col>
-              <Col lg={ 1 } md={ 2 } sm={ 4 } xs={ 4 }>
+              <Col lg={ 12 } md={ 9 } sm={ 5 } xs={ 4 } />
+              <Col lg={ 2 } md={ 3 } sm={ 5 } xs={ 4 }>
                 <Tooltip title={ t("guests.add") }>
                   <Button
+                    block
                     onClick={ () => {
                       selectedGuest(null)
                       guestDrawerOpen(true)
